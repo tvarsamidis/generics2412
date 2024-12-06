bd9f5ad2ae59844275c68d84076038fc6bbddc4b
Thomas Varsamidis <t.varsamidis@codehub.gr>
2024-12-06 12:32:02 +0200

    Create WarningDemo

--- src/gr/codehub/generics4/lecture/Box.java
+++ src/gr/codehub/generics4/lecture/Box.java
@@ -4,10 +4,15 @@ public class Box<T> {
     // T stands for "Type"
     private T t;
 
-    public void set(T t) { this.t = t; }
-    public T get() { return t; }
-    
-        public <U extends Number> void inspect(U u){
+    public void set(T t) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+
+    public <U extends Number> void inspect(U u) {
         System.out.println("T: " + t.getClass().getName());
         System.out.println("U: " + u.getClass().getName());
     }
--- src/gr/codehub/generics4/lecture/WarningDemo.java
+++ src/gr/codehub/generics4/lecture/WarningDemo.java
@@ -1,15 +1,17 @@
 package gr.codehub.generics4.lecture;
 
 public class WarningDemo {
-//    Note: WarningDemo.java uses unchecked or unsafe operations
+    //    Note: WarningDemo.java uses unchecked or unsafe operations
 //    Note: If on command line, recompile with -Xlint:unchecked for details
 //    use -Xlint:-unchecked or SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        Box<Integer> bi;
+        Box<String> bi;
         bi = createBox();
+        bi.set("aaa");
+        System.out.println(bi.get());
     }
 
-    static Box createBox(){
+    static Box createBox() {
         return new Box();
     }
 }
